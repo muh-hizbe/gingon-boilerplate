b6a3042ac9122c6f8d4ad8a9c5107de06c65b102
muh-hizbe <muh.saferagic@gmail.com>
2023-06-15 12:37:05 +0700

    fix: logging

--- configs/logging_config/logging.config.go
+++ configs/logging_config/logging.config.go
@@ -1,19 +1,35 @@
 package logging_config
 
 import (
+	"fmt"
 	"io"
 	"log"
 	"os"
+	"path/filepath"
 
 	"github.com/gin-gonic/gin"
 )
 
 var defaultPath = "logs/gin.log"
 
+func createLogFolderIfNotExist(path string) {
+	dir := filepath.Dir(path)
+	if _, err := os.Stat(dir); os.IsNotExist(err) {
+		fmt.Println("Creating", dir, "directory...")
+
+		err := os.MkdirAll(dir, 0644)
+		if err != nil {
+			fmt.Println("Fail to create ", dir, " directory", dir)
+		} else {
+			fmt.Println(dir, "directory created.")
+		}
+	}
+}
+
 func openOrCreateLogFile(path string) (*os.File, error) {
 	logFile, err := os.OpenFile(path, os.O_WRONLY|os.O_APPEND|os.O_CREATE, 0644)
 	if err != nil {
-		logFile, _ = os.Create("path")
+		logFile, _ = os.Create(defaultPath)
 	}
 	return logFile, nil
 }
@@ -25,6 +41,7 @@ func DefaultLogging(path ...string) {
 		defaultPath = path[0]
 	}
 
+	createLogFolderIfNotExist(defaultPath)
 	f, _ := openOrCreateLogFile(defaultPath)
 	gin.DefaultWriter = io.MultiWriter(f)
 	log.SetOutput(gin.DefaultWriter)
